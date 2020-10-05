af8db02f943ae9199f5c6f1f7d93d98c6b4493fa
_MR.K_ <44869790+kailasmanivannan@users.noreply.github.com>
2020-10-05 23:34:46 +0530

    Create q5.java

---
+++ week-2/q5.java
@@ -0,0 +1,10 @@
+public class Question215{ 
+    public static void main(String[] args) { 
+
+//Declare variable with name 'nptel', 'space' and 'java' and proper datatype.
+
+//Initialize the variables with proper input
+ String nptel="NPTEL",space=" ",java="JAVA";
+     System.out.print(nptel+space+java);
+   }
+}
