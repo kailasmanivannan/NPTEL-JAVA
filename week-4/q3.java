04ee0c65a3c5b12d2ad51dc8767af2f31efd41df
_MR.K_ <44869790+kailasmanivannan@users.noreply.github.com>
2020-10-25 13:36:50 +0530

    Create q3.java

---
+++ week-4/q3.java
@@ -0,0 +1,35 @@
+interface ExtraLarge{
+	 String extra = "This is extra-large";
+	 void display();
+}
+
+class Large {
+    public void Print() {
+        System.out.println("This is large");
+    }
+}
+ 
+class Medium extends Large {
+    public void Print() {
+    	  super.Print();  
+        System.out.println("This is medium");
+    }
+}
+class Small extends Medium {
+    public void Print() {
+        super.Print(); ; 
+        System.out.println("This is small");
+    }
+  }
+class Question43 implements ExtraLarge{
+  public void display(){
+    	System.out.println(extra);
+  
+  }
+    public static void main(String[] args) {
+        Small s = new Small();
+        s.Print();
+      	Question43 obj = new Question43();
+        obj.display();
+    }
+}
