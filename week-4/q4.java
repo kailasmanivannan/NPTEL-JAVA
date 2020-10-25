2c332ba4b922cf6394fb1630187a8d3b06b29cf7
_MR.K_ <44869790+kailasmanivannan@users.noreply.github.com>
2020-10-25 13:37:06 +0530

    Create q4.java

---
+++ week-4/q4.java
@@ -0,0 +1,32 @@
+interface First{ 
+    // default method 
+    default void show(){ 
+        System.out.println("Default method implementation of First interface."); 
+    } 
+} 
+  
+interface Second{ 
+    // Default method 
+    default void show(){ 
+        System.out.println("Default method implementation of Second interface."); 
+    } 
+} 
+  
+// Implementation class code 
+class Question44 implements First, Second{ 
+    // Overriding default show method 
+    public void show(){
+
+// Call show() of First interface.
+// Call show() of Second interface.
+		System.out.println("Default method implementation of First interface.");
+        System.out.println("Default method implementation of Second interface."); 
+
+
+    } 
+  
+    public static void main(String args[]){ 
+        Question44 q = new Question44(); 
+        q.show(); 
+    } 
+}
