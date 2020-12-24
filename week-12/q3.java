4f1ba52047463009b9fde2a96b1e64fd64c19b8d
_MR.K_ <44869790+kailasmanivannan@users.noreply.github.com>
2020-12-24 14:19:33 +0530

    Create q3.java

---
+++ week-12/q3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Question3{
+  public static void main(String[] args){
+      Scanner s1 = new Scanner(System.in);
+           
+String k=s1.nextLine();
+String nce=s1.nextLine();
+
+int roll=s1.nextInt();
+
+double s1nce=s1.nextDouble();
+double s2nce=s1.nextDouble();
+
+double avg=(s1nce+s2nce)/2;
+
+    }
+}
